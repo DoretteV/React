406651d5d97b58625e8a57ed119b764469ef1053
Dorette <dorette.schutte86@gmail.com>
2023-09-28 08:16:26 +0200

    changes chapt5

--- Classwork/chapter05/src/App.js
+++ Classwork/chapter05/src/App.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
-import JumboTronComponent from './JumboTronComponent';
-import Products from './Products';
+import React, { Component } from "react";
+import JumboTronComponent from "./JumboTronComponent";
+import Products from "./Products";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <JumboTronComponent />
+        <JumboTronComponent>
+          This is a long sentence, and I want to insert content into the
+          jumbotron component from the outside.
+        </JumboTronComponent>
       </div>
     );
   }
 }
 
 export default App;
-
 
 // import { Button } from "react-bootstrap";
 // import Rating from './Rating'
