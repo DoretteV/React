0f12d2f25368ec65111521a4c43bc16f881ea80d
Dorette <dorette.schutte86@gmail.com>
2023-09-29 08:22:54 +0200

    chapter7

--- Classwork/chapter07/src/JumboTronComponent.js
+++ Classwork/chapter07/src/JumboTronComponent.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Jumbotron, Button } from "react-bootstrap";
-
 class JumboTronComponent extends Component {
     constructor(props){
         super(props);
