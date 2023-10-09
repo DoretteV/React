d7496024fef706fc17c0db8e86a5b70a4fe6bd6a
Dorette <dorette.schutte86@gmail.com>
2023-10-09 08:26:23 +0200

    todoapp

---
+++ Classwork/projects/todo app/todo/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import TodoApp from "./todo/todoApp";
+
+class App extends React.Component {
+  render() {
+    return <TodoApp />;
+  }
+}
+export default App;
